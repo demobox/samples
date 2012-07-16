ac402fdaf9ce482529306a0c850fb9d505e9ecbf
Andrew Phillips <demobox1@yahoo.co.uk>
2012-07-16 13:18:20 -0400

    Added a utility to do a one-off upload of the conference data to the cloud

--- scalafx-blobstore/src/org/jclouds/conferenceapp/JcloudsUtility.scala
+++ scalafx-blobstore/src/org/jclouds/conferenceapp/JcloudsUtility.scala
@@ -1,10 +1,11 @@
 package org.jclouds.conferenceapp
 
+import com.google.common.io.ByteStreams
 import org.jclouds.ContextBuilder
-import java.util.Properties
 import org.jclouds.blobstore.BlobStoreContext
 import org.jclouds.aws.s3.AWSS3ProviderMetadata
 import java.io.InputStream
+import java.util.Properties
 
 object JcloudsUtility {
   val awsS3Metadata = AWSS3ProviderMetadata.builder().build();
@@ -27,8 +28,27 @@ object JcloudsUtility {
     val blob = blobStore.getBlob(container, resource)
     blob.getPayload.getInput
   }
-    
+
+  def storeInCloud(container:String, resource:String, contentStream: InputStream) = {
+    val blobStore = context.getBlobStore
+    // BlobBuilder.payload also accepts a stream, but then needs the length
+    val content = ByteStreams.toByteArray(contentStream)
+    blobStore.putBlob(container, blobStore.blobBuilder(resource).payload(content).build())
+  }
+
   def close() {
     context.close()
+  }
+}
+
+object ConferenceDataUploader {
+  def main(args: Array[String]) {
+    val conferenceData = getClass.getResourceAsStream("/conferenceData.zip")
+    try {
+      JcloudsUtility.storeInCloud("com.steveonjava.conferencedata", "conferenceData.zip", conferenceData)
+    } finally {
+      conferenceData.close()
+      JcloudsUtility.close()
+    }
   }
 }
